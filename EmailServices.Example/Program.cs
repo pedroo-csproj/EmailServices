156b6935c0faa73e0e61dfc701a0b1ad382042a3
pedro.octavio <pedrooctavio.dev@outlook.com>
2022-02-27 01:01:41 -0300

    DependencyInjection created and implemented

--- EmailServices.Example/Program.cs
+++ EmailServices.Example/Program.cs
@@ -1,9 +1,13 @@
+using EmailServices.DI;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddEmailServices(builder.Configuration);
 
 var app = builder.Build();
 
---
+++ EmailServices/DI/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using EmailServices.Implementations;
+using EmailServices.Interfaces;
+using EmailServices.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EmailServices.DI
+{
+    public static class DependencyInjection
+    {
+        public static void AddEmailServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<MailSettingsModel>(configuration.GetSection("MailSettings"));
+
+            services.AddTransient<IMailServices, MailServices>();
+        }
+    }
+}
